7847a90cddfb14be6a9fb5ab0a9e88e3419a9871
bogidemasrepo <bogidemas@gmail.com>
2026-01-14 10:49:26 +0300

    full project restracture

--- app/desserts/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DessertsPage = () => {
-  return <div>DessertsPage</div>;
-};
-
-export default DessertsPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,7 @@
-import Cart from "@/components/Cart";
-import Header from "@/components/Header";
-import Item from "@/components/Item";
-import data from "@/data";
-import ApiContextProvider from "@/hooks/apiContext";
-
-export default async function Home() {
+export default function Home() {
   return (
-    <ApiContextProvider>
-      <div className="w-[85vw] mx-auto pt-[100px]">
-        <Header />
-        <div className="flex flex-col min-[670px]:flex-row gap-16">
-          <div className="flex-1 flex-grow grid w-full grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 overflow-y-scroll">
-            {data.map((I) => (
-              <Item
-                key={I.name}
-                image={I.image.desktop}
-                name={I.name}
-                price={I.price}
-                catagory={I.category}
-              />
-            ))}
-          </div>
-          <Cart />
-        </div>
-      </div>
-    </ApiContextProvider>
+    <main>
+      <h1>Welcome to Desserts Web Application</h1>
+    </main>
   );
 }
